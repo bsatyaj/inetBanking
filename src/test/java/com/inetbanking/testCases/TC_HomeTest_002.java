cf905229c4ca19b4f541e235b3cae2eb4221b2a9
Satyajit <satyajitbarman72@gmail.com>
2019-12-22 12:04:13 +0530

    New test script is added

---
+++ src/test/java/com/inetbanking/testCases/TC_HomeTest_002.java
@@ -0,0 +1,15 @@
+package com.inetbanking.testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.inetbanking.pageObjects.LoginPage;
+
+public class TC_HomeTest_002 extends TC_LoginTest_001 {
+
+	@Test
+	public void HomeTest() {
+		System.out.println("This is home test");
+	}
+	
+}
